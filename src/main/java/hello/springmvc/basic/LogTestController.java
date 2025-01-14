77c60c89423c14e35f1ae6244bea516ee2180e66
jian <loveinansan1@gmail.com>
2025-01-14 18:31:54 +0900

    #4 Inflearn_springMVC1_basic
    class7_스프링 MVC_기본기능_로깅 간단히 알아보기_LogTestController 리펙토링
    
    롬복사용으로 더욱 더 편리하게 리펙토링 가

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,14 +1,14 @@
 package hello.springmvc.basic;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class LogTestController {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+    //private final Logger log = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("/log-test")
     public String logTest() {
