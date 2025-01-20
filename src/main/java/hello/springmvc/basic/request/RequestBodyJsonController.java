81f323fc10350c4975b4dc111fead1c226537f8b
jian <loveinansan1@gmail.com>
2025-01-20 15:32:27 +0900

    #28 Inflearn_springMVC1_basic
    class7_스프링 MVC_기본기능_HTTP 요청 메세지_JSON_RequestBodyJsonController_v4

--- src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,6 +49,14 @@ public class RequestBodyJsonController {
     @PostMapping("/request-body-json-v3")
     public String requestBodyJsonV3(@RequestBody HelloData helloData) throws IOException {
         log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v4")
+    public String requestBodyJsonV4(HttpEntity<HelloData> httpEntity) throws IOException {
+        HelloData data = httpEntity.getBody();
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
         return "ok";
     }
 }
